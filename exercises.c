6b3eb071938702fd863991fd7305a1436b11a960
Your Name <you@example.com>
2024-03-20 15:05:22 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -127,7 +127,7 @@ int parentesisBalanceados(char *cadena)
   Stack* P = create_stack();
 
   int i = 0;
-  while (cadena[i] != '\0'))
+  while (cadena[i] != '\0')
     {
       if (cadena[i] == '(')
       {
